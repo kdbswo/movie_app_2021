065aa6633d87a92c58f186cb88ccc6299f61ccb8
kdbswo <75184363+kdbswo@users.noreply.github.com>
2021-05-28 07:13:47 +0900

    #3.3
    planning

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import React from "react";
 
 
 class App extends React.Component{
+  state = {
+    isLoading:true,
+    movies: []
+  };
+  componentDidMount(){
+    setTimeout(()=> {
+      this.setState({ isLoading:false });
+    }, 6000);
+  }
   render() {
-    return ;
+    const {isLoading} = this.state;
+    return <div>{isLoading ? "Loading..." : "we are ready"}</div>;
   }
 }
 
