440cb7e0d30c0c88ff3b4d0a320dd8a3559f6f8e
kdbswo <75184363+kdbswo@users.noreply.github.com>
2021-05-28 05:07:36 +0900

    movieComponent

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from "react";
-import PropTypes from 'prop-types';
 
 
 class App extends React.Component{
-  state = {
-    count : 0
-  };
-  add = () =>{
-    this.setState(current => ({ count: current.count + 1}));
-  };
-  minus = () =>{
-    this.setState(current => ({ count: current.count - 1}));
-  };
-  componentDidMount(){}
   render() {
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    return ;
   }
 }
 
