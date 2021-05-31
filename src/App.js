ad1f4b0f2295477aba6aaa2da9a8faf3650911ad
kdbswo <75184363+kdbswo@users.noreply.github.com>
2021-05-31 09:58:12 +0900

    #4.0
    axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
-
+import axios from "axios";
 
 class App extends React.Component{
   state = {
     isLoading:true,
     movies: []
   };
-  componentDidMount(){
-    setTimeout(()=> {
-      this.setState({ isLoading:false });
-    }, 6000);
+  getMovies = async () => {
+    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json")
   }
+  // getMovies 가호출될때까지 기다림 (비동기함수)
+  componentDidMount(){
+    this.getMovies();
+  }
+
   render() {
     const {isLoading} = this.state;
     return <div>{isLoading ? "Loading..." : "we are ready"}</div>;
